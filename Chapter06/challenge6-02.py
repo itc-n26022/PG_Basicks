a710aa219e1a7dc4d2b2260f9e126d0daed11d4e
itc-n26022 <n26022@std.it-college.ac.jp>
2026-06-18 04:54:13 +0000

    第6章　仮締め

---
+++ Chapter06/challenge6-02.py
@@ -0,0 +1,4 @@
+what = input("何を:")
+who = input("誰に:")
+r = "私は昨日{}を書いて、{}に送った！".format(what,who)
+print(r)
---
+++ Chapter06/challenge6-08.py
@@ -0,0 +1,3 @@
+equ = "'海賊王'におれはなる"
+equ = equ.replace("'海賊王'におれはなる","Python")
+print(equ)
